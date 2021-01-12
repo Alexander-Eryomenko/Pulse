07721a8c02cc60d2d350256c7c6ba6a3ea163e41
Alexander-Eryomenko <sanyochek1991@gmail.com>
2021-01-13 00:19:40 +0200

    add tabs and cards

--- js/script.js
+++ js/script.js
@@ -6,21 +6,7 @@ $(document).ready(function(){
     nextArrow: '<button type="button" class="slick-next"><img src="img/slider/chevron-right.png"></button>',
     responsive: [
       {
-        breakpoint: 768,
-        settings: {
-          dots: true,
-          arrows: false
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          dots: true,
-          arrows: false
-        }
-      },
-      {
-        breakpoint: 480,
+        breakpoint: 992,
         settings: {
           dots: true,
           arrows: false
@@ -28,4 +14,22 @@ $(document).ready(function(){
       }
     ]
   });
+  $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+    $(this)
+      .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+      .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+  });
+
+  function toggleSlide(item) {
+    $(item).each(function(i) {
+      $(this).on('click', function(e) {
+        e.preventDefault();
+        $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+        $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+      })
+    });
+  };
+
+  toggleSlide('.catalog-item__link');
+  toggleSlide('.catalog-item__back');
 });
