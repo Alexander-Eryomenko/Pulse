252f9e821ef2ff7dd6dee58a1e2409d14d6bb603
Alexander-Eryomenko <sanyochek1991@gmail.com>
2021-01-14 17:52:54 +0200

    add section review

--- js/script.js
+++ js/script.js
@@ -4,8 +4,6 @@ $(document).ready(function(){
     adaptiveHeight: false,
     prevArrow: '<button type="button" class="slick-prev"><img src="img/slider/chevron-left.svg"></button>',
     nextArrow: '<button type="button" class="slick-next"><img src="img/slider/chevron-right.png"></button>',
-    swipeToSlide: true,
-    mobileFirst: true,
     responsive: [
       {
         breakpoint: 991,
