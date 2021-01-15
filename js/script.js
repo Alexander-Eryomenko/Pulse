ee067cbc1ca9c32777b95b4d2a9247c28370f9bc
Alexander-Eryomenko <sanyochek1991@gmail.com>
2021-01-15 21:17:50 +0200

    add mask for phone number input

--- js/script.js
+++ js/script.js
@@ -72,28 +72,36 @@ $('.button_mini').each(function(i) {
     });
 });
 
-function valideForms(form) {
+function validateForms(form){
   $(form).validate({
-    rules: {
-      name: "required",
-      phone: "required",
-      email: {
-        required: true,
-        email: true
+      rules: {
+          name: {
+              required: true,
+              minlength: 2
+          },
+          phone: "required",
+          email: {
+              required: true,
+              email: true
+          }
+      },
+      messages: {
+          name: {
+              required: "Пожалуйста, введите свое имя",
+              minlength: jQuery.validator.format("Введите {0} символа!")
+            },
+          phone: "Пожалуйста, введите свой номер телефона",
+          email: {
+            required: "Пожалуйста, введите свою почту",
+            email: "Неправильно введен адрес почты"
+          }
       }
-    },
-    messages: {
-      name: "Введите свое имя",
-      phone: "Введите свой номер телефона",
-      email: {
-        required: "Введите свою почту",
-        email: "Неправильно введен адрес почты"
-      }
-    }
   });
 };
-valideForms('#consultation-form');
-valideForms('#consultation form');
-valideForms('#order form');
 
+validateForms('#consultation-form');
+validateForms('#consultation form');
+validateForms('#order form');
+
+$('input[name=phone]').mask("+7 (999) 999-99-99");
 });
